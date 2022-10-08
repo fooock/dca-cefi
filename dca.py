6aae1acadac9ec702f5e38a0b3c4befbb69f0622
Javi <nhitbh@gmail.com>
2022-10-08 20:47:41 +0200

    Add method to retrieve exchange balance

--- dca.py
+++ dca.py
@@ -1,4 +1,5 @@
 import argparse
+import ccxt
 import signal
 import time
 import yaml
@@ -16,8 +17,31 @@ class StopBot:
         self.is_stopped = False
         signal.signal(signal.SIGINT, self.bye)
 
-    def bye(self, *args):
+    def bye(self, *args) -> None:
         self.is_stopped = True
+
+
+class Exchange:
+    """
+    Base class to interact with exchanges. Each exchange has its own
+    method to build the client, so take that into account when
+    building the exchange object.
+    """
+
+    def __init__(self, name, args) -> None:
+        exchange_keys = {"apiKey": args["apiKey"], "secret": args["secret"]}
+        exchange_class = getattr(ccxt, name)
+        self.exchange = exchange_class(exchange_keys)
+        self.exchange.set_sandbox_mode(True)
+
+    def get_balance(self) -> dict:
+        return self.exchange.fetch_balance()
+
+    def get_price(self, pairs: list) -> dict:
+        return self.exchange.fetch_tickers(pairs)
+
+    def buy(self, pair: str, position: float):
+        pass
 
 
 if __name__ == "__main__":
@@ -36,8 +60,13 @@ if __name__ == "__main__":
     with open(args.keys, "r") as f:
         keys = yaml.load(f, Loader=SafeLoader)
 
+    exchanges = [
+        Exchange(name=exchange, args=keys[exchange])
+        for exchange in strategy["strategy"][0]["exchanges"]
+    ]
     # Here we start processing our strategies based on the
     # configured period.
     bot = StopBot()
     while not bot.is_stopped:
+        usd = exchanges[0].get_balance()
         time.sleep(ONE_SECOND)
