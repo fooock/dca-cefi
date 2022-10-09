9ed90494ffd6428e34f43639c0318e31584c1cd6
Javi <nhitbh@gmail.com>
2022-10-09 16:54:58 +0200

    Add function to execute the strategy

--- dca.py
+++ dca.py
@@ -61,8 +61,56 @@ class Exchange:
             return self.name == other.name
         return False
 
+    def __repr__(self) -> str:
+        return self.exchange.name
+
+    def __hash__(self) -> int:
+        return hash(self.name)
+
+
+class Runner:
+    """
+    Runner to execute strategies.
+    """
+
+    def run(self, strategy: Strategy, exchange: Exchange):
+        # Retrieve balances in order to execute this strategy
+        balances = exchange.get_balances()
+        quote_balance = balances[strategy.quote_asset]["free"]
+        logging.info(
+            f"Available balance in {exchange.name} for '{strategy}' is {quote_balance} {strategy.quote_asset}"
+        )
+        # We are unable to execute the strategy because we don't have available
+        # balance.
+        if quote_balance < strategy.amount:
+            logging.warning(
+                f"We can't execute '{strategy}' since available funds are {quote_balance} and required amount is {strategy.amount}"
+            )
+            return
+        # If we have more than one pair, then we need to check if we have the required available
+        # balance to fill all pair orders.
+        strategy_total_amount = strategy.amount * len(strategy.get_pairs())
+        logging.info(
+            f"Required amount to execute '{strategy}' is {strategy_total_amount} {strategy.quote_asset} for pair {strategy.get_pairs()}"
+        )
+        order_pairs_to_create = []
+        aux_balance = 0
+        for pair in strategy.get_pairs():
+            if (aux_balance + strategy.amount) <= quote_balance:
+                aux_balance += strategy.amount
+                order_pairs_to_create.append(pair)
+        logging.info(
+            f"Prepared to create orders for '{strategy}' in pairs {order_pairs_to_create} for a total amount of {aux_balance}"
+        )
+        if len(order_pairs_to_create) != len(strategy.get_pairs()):
+            logging.info(
+                f"Partialy execute '{strategy}' for pairs {order_pairs_to_create} (originaly {strategy.get_pairs()})"
+            )
+        # Lets go to create orders
+
 
 if __name__ == "__main__":
+
     # Configure basic logger
     logging.basicConfig(
         format="%(asctime)s - %(levelname)s - %(message)s",
@@ -86,9 +134,9 @@ if __name__ == "__main__":
 
     strategies = [
         Strategy(
-            amount=strategy["amount"],
-            quote_asset=strategy["quote_asset"],
-            assets=strategy["assets"],
+            amount=int(strategy["amount"]),
+            quote_asset=strategy["quote_asset"].upper(),
+            assets=[asset.upper() for asset in strategy["assets"]],
             exchanges=strategy["exchanges"],
         )
         # Iterate over each strategy to create them
@@ -96,11 +144,22 @@ if __name__ == "__main__":
     ]
     for strategy in strategies:
         logging.info(
-            f"Running '{strategy}' for pairs {strategy.get_pairs()} into exchanges {strategy.exchanges}"
+            f"Detected '{strategy}' for pairs {strategy.get_pairs()} into exchanges {strategy.exchanges}"
         )
-
+    # Build exchange objects based on the ones found in strategies.
+    # These ones are being used inside the strategy runner.
     exchanges = [
         Exchange(name=exchange, keys=read_keys[exchange], test=args.test)
         for strategy in strategies
         for exchange in strategy.exchanges
     ]
+    exchanges = list(set(exchanges))
+    logging.info(f"Created {len(exchanges)} exchanges: {exchanges}")
+
+    runner = Runner()
+    for strategy in strategies:
+        for exchange in exchanges:
+            if exchange.name not in strategy.exchanges:
+                continue
+            # Execute strategy in this exchange
+            runner.run(strategy, exchange)
